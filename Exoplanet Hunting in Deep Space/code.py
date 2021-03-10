dc5b8e6fa1dcceefb9bb5cf56d3e01f44cf6e44d
vigneshviggs <vigneshviggs100@gmail.com>
2021-03-09 19:59:50 -0500

    Updated the code to correct the input dimensions
    Have not tested on more epochs yet.
    
    Link: https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.uniform_filter1d.html
    https://stackoverflow.com/questions/55207719/cant-understand-the-working-of-uniform-filter1d-function-imported-from-scipy

--- Exoplanet Hunting in Deep Space/code.py
+++ Exoplanet Hunting in Deep Space/code.py
@@ -15,9 +15,13 @@ from keras.models import Sequential
 from keras.layers import Conv1D, MaxPool1D, Dense, Dropout, Flatten, BatchNormalization, Input, concatenate, Activation
 import matplotlib.pyplot as plt
 from keras.optimizers import Adam
+from sklearn.linear_model import LinearRegression
+from scipy.ndimage.filters import uniform_filter1d
 
 train_df = pd.read_csv('exoTrain.csv')
 test_df = pd.read_csv('exoTest.csv')
+print(train_df.shape)
+print(test_df.shape)
 
 x_train = train_df.iloc[:,1:]
 y_train = train_df["LABEL"] - 1
@@ -28,15 +32,22 @@ y_test = test_df["LABEL"] - 1
 
 min_max_scaler = preprocessing.MinMaxScaler()
 x_train = min_max_scaler.fit_transform(x_train)
+print(x_train)
+print(x_train.shape)
+a=x_train[0,:]
+
 
 x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=0.5)
 
-print(x_train.shape)               
-#(5087, 3197)
-print(x_train.shape[1:])
+xx_train = np.stack([x_train, uniform_filter1d(x_train, axis=1, size=200)], axis=2)
+xx_test = np.stack([x_test, uniform_filter1d(x_test, axis=1, size=200)], axis=2) 
+
+hist = LinearRegression().fit(x_train, y_train)
+hist.predict(x_test)
+
 
 model = Sequential()
-model.add(Conv1D(input_shape=(3197,), filters=8, kernel_size=11, activation='relu'))
+model.add(Conv1D(input_shape=(3197, 2), filters=8, kernel_size=11, activation='relu'))
 model.add(MaxPool1D(strides=4))
 model.add(BatchNormalization())
 model.add(Conv1D(filters=16, kernel_size=11, activation='relu'))
@@ -60,10 +71,10 @@ hist = model.fit(x_train, y_train,
                            verbose=0, epochs=5)
 
 plt.plot(hist.history['loss'], color='b')
-plt.plot(hist.history['val_loss'], color='r')
+#plt.plot(hist.history['val_loss'], color='r')
 plt.show()
-plt.plot(hist.history['acc'], color='b')
-plt.plot(hist.history['val_acc'], color='r')
+plt.plot(hist.history['accuracy'], color='b')
+#plt.plot(hist.history['val_acc'], color='r')
 plt.show()
 
 
